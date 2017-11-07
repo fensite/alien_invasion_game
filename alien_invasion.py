4417b260adcc828125c234213d82f7efbd2adcc8
zhai.kangning <zhai.kangning@icntv.tv>
2017-11-07 17:19:40 +0800

    消灭外星人小游戏

---
+++ alien_invasion.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import pygame
+
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    #初始化游戏并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #创造一艘飞船
+    ship = Ship(ai_settings, screen)
+
+    # 创造一个外星人编组和一个子弹编组
+    aliens = Group() 
+
+    bullets = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+
+    #开始游戏主循环
+    while True:
+ 
+        #监视键盘和鼠标事件
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+      
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+
+
+run_game()
